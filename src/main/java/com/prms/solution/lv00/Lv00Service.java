2bd421a1da992cb9317348396c2c36708a293efd
jeongzmin <jeongzmin@plateer.com>
2022-12-20 13:00:33 +0900

    logback 설정완료

--- src/main/java/com/prms/solution/lv00/Lv00Service.java
+++ src/main/java/com/prms/solution/lv00/Lv00Service.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class Lv00Service {
 
-	public void lv() {
-		
-		System.out.println("메ㅔㅔㅔㅔ");
-		
+//	입력값 〉	1, 2, 3, 4
+//	기댓값 〉	[5, 4]
+	public int[] 분수의덧셈(int denum1, int num1, int denum2, int num2) {
+		int[] answer = {denum1*num2	+	denum2*num1, num1*num2	+	num2*num1};
+//		answer = 
+//		denum1*num2	+	denum2*num1
+//		num1*num2	+	num2*num1
+		return answer;
 	}
 
 }
--- src/test/java/com/prms/solution/lv00/SolutionTestTests.java
+++ src/test/java/com/prms/solution/lv00/SolutionTestTests.java
@@ -1,22 +1,46 @@
 package com.prms.solution.lv00;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("JUnit Exam")
 class SolutionTestTests {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private Lv00Service lv00Service;
+	
+	private static Stream<Arguments> getParameter() {
+	    return Stream.of(
+	      Arguments.of(1, 2, 3, 4),
+	      Arguments.of(9, 2, 1, 3)
+	    );
+	  }
 
-	@Test
-	void contextLoads() {
+//	@DisplayName("분수의덧셈 Tests")
+	@ParameterizedTest(name ="{index} {displayName} message = {0}")
+//	@ParameterizedTest(name ="{index} {displayName} message = {0}")
+	@MethodSource("getParameter")
+//	@NullSource
+//	@EmptySource
+	void 분수의덧셈(int denum1, int num1 , int denum2, int num2) throws Exception{
 		
-		System.out.println(" test  test  test  test  test  test  test  test ");
+		int[] answer = {};
 		
-		lv00Service.lv();
+		answer = lv00Service.분수의덧셈( denum1, num1, denum2, num2 );
+		//		System.out.println(answer);
+		
+		System.out.println("결과 : "+denum1);
 		
 	}
-
 }
